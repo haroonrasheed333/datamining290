ad78df8acc47e26f71e18fd8bfa70b8ab785f8c4
Haroon Rasheed <haroon@ischool.berkeley.edu>
2013-02-21 21:25:40 -0800

    mapreduce

--- code/user_similarity.py
+++ code/user_similarity.py
@@ -1,24 +1,38 @@
 from mrjob.job import MRJob
 from mrjob.protocol import JSONValueProtocol
+from itertools import combinations
 
 class UserSimilarity(MRJob):
     INPUT_PROTOCOL = JSONValueProtocol
 
     ###
     # TODO: write the functions needed to
-    # 1) find potential matches, 
+    # 1) find potential matches,
     # 2) calculate the Jaccard between users, with a user defined as a set of
     # reviewed businesses
     ##/
 
+    def user_business(self, _, record):
+        if record['type'] == 'review':
+            yield [record['user_id'], record['business_id']]
+
+    def user_businesses(self, userid, businessid):
+        business_id = set(businessid)  # set() uniques an iterator
+        yield ["Comb", [userid, list(business_id)]]
+
+    def similarity(self, stat, usbslist):
+        k = list(usbslist)
+        j = combinations(k, 2)
+        for line in list(j):
+            num = len(set(line[0][1]) & set(line[1][1]))
+            den = len(set(line[0][1]) | set(line[1][1]))
+            sim = num / (float(den))
+            if sim>=0.5:
+                yield[[line[0][0],line[1][0]],sim]
+
     def steps(self):
-        """TODO: Document what you expect each mapper and reducer to produce:
-        mapper1: <line, record> => <key, value>
-        reducer1: <key, [values]>
-        mapper2: ...
-        """
-        return [self.mr(mapper=self.mapper1, reducer=self.reducer1),
-                self.mr(mapper=...)]
+        return [self.mr(self.user_business, self.user_businesses),
+                self.mr(reducer=self.similarity)]
 
 
 if __name__ == '__main__':
