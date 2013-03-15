921342e60894409161bf41860c3a3f5291fcae47
Haroon Rasheed <haroon@ischool.berkeley.edu>
2013-03-14 23:50:20 -0700

    hw-kmeans

--- code/k_means.py
+++ code/k_means.py
@@ -2,6 +2,7 @@
 # Implement simple k-means clustering using 1 dimensional data
 #
 ##/
+import random
 
 dataset = [-13.65089255716321, -0.5409562932238607, -88.4726466247223, 39.30158828358612, 4.066458182574449, 64.64143300482378, 38.68269424751338, 33.42013676314311, 31.18603331719732, -0.2027616409406292, 45.13590038987272, 30.791899783552395, 61.1727490302448, 18.167220741624856, 88.88077709786394, -1.3808002119514704, 50.14991362212521, 55.92029956281276, -6.759813255299466, 34.28290084421072]
 k = 2 # number of clusters
@@ -13,23 +14,25 @@ k = 2 # number of clusters
 
 def pick_centroids(xs, num):
     """Return list of num centroids given a list of numbers in xs"""
-    ###
-    # TODO select and return centroids
-    return [1,2]
-    ##/
+    icentroids = []
+    i = 0
+    while (i < num):
+        index = random.randint(0, len(xs) - 1)
+        icentroids.append(xs[index])
+        i = i + 1
+
+    return icentroids
 
 def distance(a, b):
     """Return the distance of numbers a and b"""
-    ###
-    # TODO return correct expression
-    return 0
-    ##/
+    dist = abs(a - b)
+    return dist
 
 def centroid(xs):
     """Return the centroid number given a list of numbers, xs"""
-    ###
+    cent = sum(xs) / len(xs)
     # TODO calculate and return centroid
-    return 0
+    return cent
     ##/
 
 def cluster(xs, centroids):
